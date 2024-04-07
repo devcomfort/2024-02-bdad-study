6f9a9ecc1b2bb832b37324f02fac62698750a2b7
devcomfort <kimdonghyun026@gmail.com>
2024-04-07 19:42:07 +0000

    feaet : Orientation PPT 추가

---
+++ src/routes/ppts/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	// Google Fonts
+	import GoogleFont, { getFontStyle } from '@svelte-web-fonts/google';
+	import type { GoogleFontFamily, GoogleFontVariant } from '@svelte-web-fonts/google';
+
+	// Reveal.js
+	import Reveal from 'reveal.js';
+	// Reveal.js Styles
+	import 'reveal.js/dist/reveal.css';
+	import 'reveal.js/dist/theme/black.css';
+	// Reveal.js Built-in Plugins
+	import Highlight from 'reveal.js/plugin//highlight/highlight';
+	import Notes from 'reveal.js/plugin/notes/notes';
+
+	const font = {
+		family: 'Noto Sans KR' as GoogleFontFamily,
+		variants: ['400'] as GoogleFontVariant[]
+	} as const;
+
+	onMount(function () {
+		const deck = new Reveal({
+			plugins: [Highlight, Notes]
+		});
+
+		deck.initialize({
+			dependencies: []
+		});
+	});
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<GoogleFont fonts={[font]} display="swap" />
+</svelte:head>
+
+<div class="reveal" style={getFontStyle(font.family, font.variants[0])}>
+	<div class="slides">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/ppts/1/+page.svelte
@@ -0,0 +1,50 @@
+<section>
+	<h1 class="r-fit-text">🎉 웹 기초 스터디 #1</h1>
+
+	<p>Orientation</p>
+
+	<!-- <aside class="notes">Shhh, these are your private notes 📝</aside> -->
+</section>
+
+<section>
+	<h1>🗓️ 스터디 일정</h1>
+
+	<ul>
+		<li class="fragment">총 38 시간, 1주일 2회, 월, 화</li>
+		<li class="fragment fade-in">
+			교내 행사, 공휴일 등에 따라 일정 변경 시 디스코드/카카오톡 공지
+		</li>
+		<li class="fragment fade-in">⚠️ 행정 처리를 위해 결석 시 사유 알림 필요</li>
+	</ul>
+</section>
+
+<section>
+	<h1>🏫 스터디 장소</h1>
+
+	<ul>
+		<li class="fragment">
+			오프라인: <span class="fragment fade-in">뉴밀레니엄관 2층 상상스퀘어, 학교 주변 카페</span>
+		</li>
+		<li class="fragment">
+			온라인: <span class="fragment fade-in">디스코드 (화면 공유)</span>
+		</li>
+	</ul>
+</section>
+
+<section>
+	<h1>📝 스터디 내용</h1>
+
+	<ul style="font-size: xx-large">
+		<li class="fragment">스터디 내용: HTML, CSS, JavaScript, TypeScript</li>
+		<li class="fragment">
+			교재: <i>Do it! HTML+CSS+자바스크립트 웹 표준의 정석</i>
+		</li>
+		<li class="fragment">진도가 일찍 끝나면 원하는 공부나 개인 프로젝트 수행할 계획</li>
+	</ul>
+</section>
+
+<section>
+	<h1>📱 연락 방법</h1>
+
+	<p class="fragment">질문은 010-2412-6586 또는 카카오톡으로</p>
+</section>
