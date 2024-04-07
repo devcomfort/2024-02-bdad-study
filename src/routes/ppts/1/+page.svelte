367fd96197bb7d7adc10dffc104bb879586d18ad
devcomfort <kimdonghyun026@gmail.com>
2024-04-07 23:35:56 +0000

    feat : 가독성을 위한 디자인 조정, 누락된 내용 추가

--- src/routes/ppts/1/+page.svelte
+++ src/routes/ppts/1/+page.svelte
@@ -9,9 +9,9 @@
 <section>
 	<h1>🗓️ 스터디 일정</h1>
 
-	<ul>
+	<ul class="text-4xl">
 		<li class="fragment">총 38 시간, 1주일 2회, 월, 화</li>
-		<li class="fragment fade-in">
+		<li class="fragment fade-in text-3xl">
 			교내 행사, 공휴일 등에 따라 일정 변경 시 디스코드/카카오톡 공지
 		</li>
 		<li class="fragment fade-in">⚠️ 행정 처리를 위해 결석 시 사유 알림 필요</li>
@@ -21,7 +21,7 @@
 <section>
 	<h1>🏫 스터디 장소</h1>
 
-	<ul>
+	<ul class="text-4xl">
 		<li class="fragment">
 			오프라인: <span class="fragment fade-in">뉴밀레니엄관 2층 상상스퀘어, 학교 주변 카페</span>
 		</li>
@@ -34,13 +34,22 @@
 <section>
 	<h1>📝 스터디 내용</h1>
 
-	<ul style="font-size: xx-large">
+	<ul class="text-4xl">
 		<li class="fragment">스터디 내용: HTML, CSS, JavaScript, TypeScript</li>
 		<li class="fragment">
 			교재: <i>Do it! HTML+CSS+자바스크립트 웹 표준의 정석</i>
 		</li>
-		<li class="fragment">진도가 일찍 끝나면 원하는 공부나 개인 프로젝트 수행할 계획</li>
+		<li class="fragment text-3xl">진도가 일찍 끝나면 원하는 공부나 개인 프로젝트 수행할 계획</li>
 	</ul>
+</section>
+
+<section>
+	<h1>❗ 중요</h1>
+
+	<p class="fragment text-2xl break-words">
+		스터디 수행 인증을 위해 매 온/오프라인 스터디마다<br>
+		모든 참여자의 얼굴이 보이는 사진 한 장을 타임 스탬프가 보이도록 촬영해야 합니다
+	</p>
 </section>
 
 <section>
