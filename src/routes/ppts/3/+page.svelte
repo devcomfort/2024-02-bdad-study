cdb7701619b0e2b9f0dbcc7591826330804e2ca3
devcomfort <kimdonghyun026@gmail.com>
2024-04-09 08:34:54 +0000

    feat : HTML 기본 문서 구조 추가

---
+++ src/routes/ppts/3/+page.svelte
@@ -0,0 +1,41 @@
+<section>
+	<h2>HTML 문서 기본 구조</h2>
+
+	<pre><code data-trim data-noescape class="language-html" data-line-numbers="|1|2,12|3,6|7,11">
+      <script type="text/template">
+      <!DOCTYPE HTML> <!-- 문서 버전 -->
+      <html lang="ko"> <!-- HTML 문서 시작 (언어 정보: ko) -->
+        <head> <!-- 문서 세부 정보 시작 -->
+          <meta charset="UTF-8" /> <!-- 인코딩 정보 제공 -->
+          <title>제목</title> <!-- 페이지 제목 제공 -->
+        </head> <!-- 문서 세부 정보 끝 -->
+        <body> <!-- 문서 시각 정보 시작 -->
+          <h1>웹 개발 스터디</h1> <!-- 제목 (header) -->
+          <hr> <!-- 구분선, 수평선 (horizontal rule) -->
+          <p>HTML, CSS, JS, TS</p> <!-- 문장 (절, paragraph) -->
+        </body> <!-- 문서 시각 정보 끝 -->
+      </html> <!-- HTML 문서 끝 -->
+			</script>
+		</code></pre>
+</section>
+
+<section>
+	<h2>HTML 문서 내용</h2>
+
+	<pre><code data-trim data-noescape class="language-html" data-line-numbers="|4|5|8|9|10">
+       <script type="text/template">
+      <!DOCTYPE HTML> <!-- 문서 버전 -->
+      <html lang="ko"> <!-- HTML 문서 시작 (언어 정보: ko) -->
+        <head> <!-- 문서 세부 정보 시작 -->
+          <meta charset="UTF-8" /> <!-- 인코딩 정보 제공 -->
+          <title>제목</title> <!-- 페이지 제목 제공 -->
+        </head> <!-- 문서 세부 정보 끝 -->
+        <body> <!-- 문서 시각 정보 시작 -->
+          <h1>웹 개발 스터디</h1> <!-- 제목 (header) -->
+          <hr> <!-- 구분선, 수평선 (horizontal rule) -->
+          <p>HTML, CSS, JS, TS</p> <!-- 문장 (절, paragraph) -->
+        </body> <!-- 문서 시각 정보 끝 -->
+      </html> <!-- HTML 문서 끝 -->
+			</script>
+		</code></pre>
+</section>
