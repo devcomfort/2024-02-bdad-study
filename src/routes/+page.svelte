38a8e2fcb649a99f9284fbe098532f31fed33f9f
devcomfort <kimdonghyun026@gmail.com>
2024-04-09 03:39:44 +0000

    feat : 두번째 PPT 라우팅 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
 	import '@picocss/pico/css/pico.css';
-  import {format} from 'date-fns'
+	import { format } from 'date-fns';
 
-  const pptList = [
-    {
-      title:"Orientation",
-      body: "수업 관련 안내 사항 전달",
-      date: new Date("2024-04-08"),
-      link: "./ppts/1"
-    }
-  ] as const;
+	interface Profile {
+		title: string;
+		body: string;
+		date: Date;
+		link: string;
+	}
+
+	const pptList: Profile[] = [
+		{
+			title: 'Orientation',
+			body: '수업 관련 안내 사항 전달',
+			date: new Date('2024-04-08'),
+			link: './ppts/1'
+		},
+		{
+			title: '개발 환경 설정법',
+			body: 'VSCode와 확장 프로그램',
+			date: new Date('2024-04.09'),
+			link: './ppts/2'
+		}
+	];
 </script>
 
 <div>
-	<main class="container fluid-width">
-    <hgroup>
-      <p class="font-bold text-4xl">PPT List</p>
-      <p>수업 PPT 몰아보기</p>
-    </hgroup>
+	<main class="fluid-width container">
+		<hgroup>
+			<p class="text-4xl font-bold">PPT List</p>
+			<p>수업 PPT 몰아보기</p>
+		</hgroup>
 
-    <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>이름</th>
-          <th>내용</th>
-          <th>업로드 날짜</th>
-          <th>링크</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each pptList as ppt, index}
-        <tr>
-          <td>{index}</td>
-          <td>{ppt.title}</td>
-          <td>{ppt.body}</td>
-          <td>{format(ppt.date, "yyyy.MM.dd")}</td>
-          <td>
-            <a href="{ppt.link}" data-sveltekit-reload>접속하기</a>
-          </td>
-        </tr>
-        {/each}
-      </tbody>
-    </table>
-  </main>
+		<table>
+			<thead>
+				<tr>
+					<th></th>
+					<th>이름</th>
+					<th>내용</th>
+					<th>업로드 날짜</th>
+					<th>링크</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each pptList as ppt, index}
+					<tr>
+						<td>{index}</td>
+						<td>{ppt.title}</td>
+						<td>{ppt.body}</td>
+						<td>{format(ppt.date, 'yyyy.MM.dd')}</td>
+						<td>
+							<a href={ppt.link} data-sveltekit-reload>접속하기</a>
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</main>
 </div>
